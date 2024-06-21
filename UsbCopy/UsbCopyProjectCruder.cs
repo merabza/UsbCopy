f5a54d2f986be16d3d3595fd176ae4e2e2698706
merabza <merabza@gmail.com>
2024-06-21 15:33:38 +0400

    202406211533

--- UsbCopy/UsbCopyProjectCruder.cs
+++ UsbCopy/UsbCopyProjectCruder.cs
@@ -62,7 +62,7 @@ public sealed class UsbCopyProjectCruder : ParCruder
         projects.Remove(recordKey);
     }
 
-    protected override ItemData CreateNewItem(ItemData? defaultItemData)
+    protected override ItemData CreateNewItem(string? recordKey, ItemData? defaultItemData)
     {
         return new UsbCopyProjectModel();
     }
